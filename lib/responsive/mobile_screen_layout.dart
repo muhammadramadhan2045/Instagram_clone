aa51d8630244a5c8424bca32662e371f5b5b0015
Muhammad Ramadhan <muhammadramadhan2045@gmail.com>
2023-01-22 16:34:24 +0700

    mobile bottom app bar

--- lib/responsive/mobile_screen_layout.dart
+++ lib/responsive/mobile_screen_layout.dart
@@ -1,9 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:instagrams_flutter/models/user.dart' as model;
 import 'package:instagrams_flutter/providers/user_provider.dart';
+import 'package:instagrams_flutter/utils/colors.dart';
 import 'package:provider/provider.dart';
 
 class MobileScreenLayout extends StatefulWidget {
@@ -14,34 +16,94 @@ class MobileScreenLayout extends StatefulWidget {
 }
 
 class _MobileScreenLayoutState extends State<MobileScreenLayout> {
-  String username = "";
+  int _page = 0;
+  late PageController pageController;
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    getUsername();
+    pageController = PageController();
   }
 
-  void getUsername() async {
-    DocumentSnapshot snap = await FirebaseFirestore.instance
-        .collection('users')
-        .doc(FirebaseAuth.instance.currentUser!.uid)
-        .get();
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    pageController.dispose();
+  }
 
-    print(snap.data());
+  void navigationOnTap(int page) {
+    pageController.jumpToPage(page);
+  }
+
+  void onPageChanges(int page) {
     setState(() {
-      username = (snap.data() as Map<String, dynamic>)['username'];
+      _page = page;
     });
-    print(username);
   }
 
   @override
   Widget build(BuildContext context) {
     model.User? user = Provider.of<UserProvider>(context).getUser;
     return Scaffold(
-      body: Center(
-        child: Text(user.username),
+      body: PageView(
+        children: [
+          const Center(
+            child: Text("Rama"),
+          ),
+          const Center(
+            child: Text("Aulia"),
+          ),
+          const Center(
+            child: Text("Agrina"),
+          ),
+          const Center(
+            child: Text("Asep "),
+          ),
+          const Center(
+            child: Text("Tirta"),
+          ),
+        ],
+        physics: const NeverScrollableScrollPhysics(),
+        controller: pageController,
+        onPageChanged: onPageChanges,
+      ),
+      bottomNavigationBar: CupertinoTabBar(
+        backgroundColor: mobileBackgroundColor,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home,
+                color: _page == 0 ? primaryColor : secondaryColor),
+            label: '',
+            backgroundColor: primaryColor,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search,
+                color: _page == 1 ? primaryColor : secondaryColor),
+            label: '',
+            backgroundColor: primaryColor,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_circle,
+                color: _page == 2 ? primaryColor : secondaryColor),
+            label: '',
+            backgroundColor: primaryColor,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite,
+                color: _page == 3 ? primaryColor : secondaryColor),
+            label: '',
+            backgroundColor: primaryColor,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_pin,
+                color: _page == 4 ? primaryColor : secondaryColor),
+            label: '',
+            backgroundColor: primaryColor,
+          ),
+        ],
+        onTap: navigationOnTap,
       ),
     );
   }
